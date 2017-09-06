ec4ba94c5f4bb769a8267e4bf39ba227f65f8b47
Daniel Patterson <dan24patt@gmail.com>
2017-09-06 22:39:16 +0100

    Add tests for range

--- src/utils/range.spec.ts
+++ src/utils/range.spec.ts
@@ -1,9 +1,31 @@
 import 'mocha';
 import { expect } from 'chai';
-import { range } from './range';
+import { range, inclusiveRange } from './range';
 
 describe('range', () => {
-  it('should return all the integers from the first to the last - 1', () => {
+  it('should return all the integers from the start to the end - 1', () => {
     expect(range(1, 4)).to.deep.equal([1, 2, 3]);
+  });
+
+  it('should return an empty array if the end is less than the start', () => {
+    expect(range(1, 0)).to.deep.equal([]);
+  });
+
+  it('should return an empty array if the end is the start', () => {
+    expect(range(1, 1)).to.deep.equal([]);
+  });
+});
+
+describe('inclusiveRange', () => {
+  it('should return all the integers from the start to the end', () => {
+    expect(inclusiveRange(1, 4)).to.deep.equal([1, 2, 3, 4]);
+  });
+
+  it('should return an empty array if the end is less than the start', () => {
+    expect(inclusiveRange(1, 0)).to.deep.equal([]);
+  });
+
+  it('should return an array with just the start if the end is the start', () => {
+    expect(inclusiveRange(1, 1)).to.deep.equal([1]);
   });
 });
