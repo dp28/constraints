ba323b06a7033208845c5d69f9a29838a1d98448
Daniel Patterson <dan24patt@gmail.com>
2017-09-20 08:20:57 +0100

    Add tests for NewEventContainer

---
+++ event-schedule-app/src/components/NewEvent/NewEventContainer.test.js
@@ -0,0 +1,31 @@
+import { mapStateToProps } from "./NewEventContainer";
+
+describe("mapStateToProps", () => {
+  function mockState({ config = {}, newEvent = {} } = {}) {
+    return { config, newEvent };
+  }
+  it("should map out the config property 'minutesPerUnit'", () => {
+    expect(
+      mapStateToProps(mockState({ config: { minutesPerUnit: 2 } }))
+        .minutesPerUnit
+    ).toEqual(2);
+  });
+
+  it("should map out the config property 'pixelsPerUnit'", () => {
+    expect(
+      mapStateToProps(mockState({ config: { pixelsPerUnit: 2 } })).pixelsPerUnit
+    ).toEqual(2);
+  });
+
+  ["minStart", "minDuration", "maxDuration", "maxEnd"].forEach(property => {
+    it(`should map out the newEvent property '${property}'`, () => {
+      expect(
+        mapStateToProps(mockState({ newEvent: { [property]: 2 } }))[property]
+      ).toEqual(2);
+    });
+
+    it(`should set '${property}' to 0 if it does not exist on newEvent`, () => {
+      expect(mapStateToProps(mockState())[property]).toEqual(0);
+    });
+  });
+});
