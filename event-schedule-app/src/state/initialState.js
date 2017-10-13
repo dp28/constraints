56a98b84135b2612f698747db74b613536aa9f0e
Daniel Patterson <dan24patt@gmail.com>
2017-10-13 18:59:20 +0100

    Merge saved state with default on startup
    So that newly added state properties are correctly inserted
    without losing the current saved state.

--- event-schedule-app/src/state/initialState.js
+++ event-schedule-app/src/state/initialState.js
@@ -1,17 +1,22 @@
-import { load } from "./localStorageRepository";
+import deepmerge from "deepmerge";
 
+import { load } from "./localStorageRepository";
 import * as config from "../components/Config/ConfigReducer";
 import * as constrainedEvents from "../components/Events/EventsReducer";
 import * as drag from "../components/Drag/DragReducer";
 
+const InitialState = {
+  config: config.InitialState,
+  constrainedEvents: constrainedEvents.InitialState,
+  drag: drag.InitialState
+};
+
 export function buildInitialState() {
-  return load() || buildInitialStateFromReducers();
+  const savedState = load();
+  const defaultState = buildInitialStateFromReducers();
+  return savedState ? deepmerge(defaultState, savedState) : defaultState;
 }
 
 export function buildInitialStateFromReducers() {
-  return {
-    config: config.InitialState,
-    constrainedEvents: constrainedEvents.InitialState,
-    drag: drag.InitialState
-  };
+  return InitialState;
 }
--- event-schedule-app/src/state/initialState.test.js
+++ event-schedule-app/src/state/initialState.test.js
@@ -4,10 +4,53 @@ import { save, load } from "./localStorageRepository";
 describe("buildInitialState", () => {
   describe("if there is state saved in localStorage", () => {
     afterEach(() => localStorage.clear());
-
-    it("should return that state", () => {
-      save({ some: "state" });
-      expect(buildInitialState()).toEqual({ some: "state" });
+    it("should merge the default initial state with the saved state, keeping saved state if it exists", () => {
+      const saved = {
+        config: {
+          minutesPerUnit: 15,
+          pixelsPerUnit: 1
+        },
+        constrainedEvents: {
+          events: {
+            a: { id: "a", name: "saved event" }
+          }
+        },
+        drag: {
+          startUnits: null,
+          startPixels: null
+        }
+      };
+      save(saved);
+      const current = {
+        config: {
+          minutesPerUnit: 15,
+          pixelsPerUnit: 5,
+          bounds: { min: 0, max: 96 }
+        },
+        constrainedEvents: {
+          events: {}
+        },
+        drag: {
+          startUnits: null,
+          startPixels: null
+        }
+      };
+      expect(buildInitialState()).toEqual({
+        config: {
+          minutesPerUnit: 15,
+          pixelsPerUnit: 1,
+          bounds: { min: 0, max: 96 }
+        },
+        constrainedEvents: {
+          events: {
+            a: { id: "a", name: "saved event" }
+          }
+        },
+        drag: {
+          startUnits: null,
+          startPixels: null
+        }
+      });
     });
   });
 
